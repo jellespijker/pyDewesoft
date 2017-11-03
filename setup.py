d5fe131491547cb02da6aceecfd92ca427b20753
Jelle Spijker <j.spijker@ihcmti.com>
2017-11-03 17:35:50 +0100

    setup and requirements added

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyDewesoft',
+    version='0.1a',
+    url='http://mti-gitlab.ihc.eu/generic-software/pyDewesoft',
+    license='MIT License',
+    author='Jelle Spijker',
+    author_email='j.spijker@ihcmti.com',
+    description='A Python module to read Dewesoft datafiles',
+    keywords='Measurement, Engineering, DSP, Signal processing',
+    packages=find_packages(),
+    install_requires=['pint', 'numpy', 'pip-tools']
+)
