ff2ddb36aca7d038eec0c1a5fe84952081d8df05
Jelle Spijker <j.spijker@ultimaker.com>
2020-01-17 14:06:11 +0100

    Update requirements en setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyDewesoft',
-    version='1.0rc0',
+    version='1.1a0',
     url='http://mti-gitlab.ihc.eu/generic-software/pyDewesoft',
     license='MIT License',
     author='Jelle Spijker',
@@ -10,6 +10,6 @@ setup(
     description='A Python module to read Dewesoft datafiles',
     keywords='Measurement, Engineering, DSP, Signal processing',
     packages=find_packages(),
-    install_requires=['pint', 'numpy', 'pip-tools', 'dill'],
+    install_requires=['pint', 'numpy', 'dill'],
     include_package_data=True
 )
